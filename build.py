6fea41ee59442e29d607d8438bb808ae701b4749
Marko Mitosevic <marko.mitosevic@jetbrains.com>
2025-01-23 15:58:21 +0000

    Add github action linux/aarch64 build
    * added release environment
    * parameterised cache bucket name
    * parameterised python repository

--- build.py
+++ build.py
@@ -1,5 +1,6 @@
 import platform
 import sys
+import importlib
 
 def get_plat_name():
     if platform.machine() == "aarch64":
@@ -8,5 +9,9 @@ def get_plat_name():
         return "manylinux_2_31_x86_64"
 
 if __name__ == "__main__":
-    plat_name = get_plat_name()
-    sys.exit(plat_name)
+    setuptools = importlib.import_module('setuptools')
+    setuptools.setup(
+        bdist_wheel={
+            'plat_name': get_plat_name(),
+        },
+    )
