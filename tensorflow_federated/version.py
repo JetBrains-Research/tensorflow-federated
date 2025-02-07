a35cdf5579d1926ef95eed5119d2cd49001042f6
Marko Mitosevic <marko.mitosevic@jetbrains.com>
2025-02-07 14:31:46 +0000

    Add plat-name python script
    * added plat-name python script
    * removed plat-name script from build_python_package.sh

--- tensorflow_federated/version.py
+++ tensorflow_federated/version.py
@@ -13,4 +13,59 @@
 # limitations under the License.
 """TensorFlow Federated version."""
 
-__version__ = '0.86.3a'
+import platform
+import os
+import sys
+import re
+
+def get_glibc_version():
+    if sys.platform.startswith('linux'):
+        try:
+            output = os.popen('ldd --version 2>&1').readline()
+            match = re.search(r"(\d+(?:\.\d+)+)", output)
+            if match:
+                return match.group(1).replace('.', '_').strip()
+
+            return None
+
+        except Exception as e:
+            print(f"Error detecting glibc version: {e}")
+            return  None
+    else:
+        return None
+
+
+def get_system_architecture():
+    arch = platform.machine()
+    if arch == "x86_64" or arch == "amd64":
+        return "x86_64"
+    elif arch == "i386" or arch == "i686":
+        return "x86"
+    elif arch.startswith('armv'):
+        return "arm"
+    elif arch.startswith('aarch64') or arch == "arm64":
+        return "aarch64"
+    elif arch.startswith('ppc'):
+        return "ppc"
+    elif arch.startswith('s390'):
+        return "s390"
+    return arch
+
+def get_platform_name():
+    system = platform.system()
+    arch = get_system_architecture()
+
+    if system == "Linux":
+        glibc_version = get_glibc_version()
+        if glibc_version:
+            return f"manylinux_{glibc_version}_{arch}"
+
+    return f"unknown_{arch}"
+
+__plat_name__ = get_platform_name()
+__version__ = '0.86.3b'
+
+if __name__ == "__main__":
+    print(f"__plat_name__ = {__plat_name__}")
+
+
